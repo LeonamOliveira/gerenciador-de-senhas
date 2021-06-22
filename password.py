8a85ca968017e2bed8d2288a20d8484baa17d0ad
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-06-22 19:04:17 -0300

    Validando senha padrão

--- password.py
+++ password.py
@@ -2,6 +2,11 @@ import sqlite3
 
 MASTER_PASSWORD = "123456"
 
+senha = input("Insira sua senha master: ")
+if senha != MASTER_PASSWORD:
+    print("Senha inválida! Encerrando...")
+    exit()
+
 conn = sqlite3.connect('passwords.db')
 
 cursor = conn.cursor()
@@ -12,3 +17,25 @@ cursor.execute('''
         password TEXT NOT NULL
     );
 ''')
+
+
+def menu():
+    print("********************************")
+    print("* I: Inserir uma nova senha:   *")
+    print("* L: Listas serviços salvos:   *")
+    print("* R: Recuperar uma senha       *")
+    print("* S: Sair                      *")
+    print("********************************")
+
+
+while True:
+    menu()
+    op = input("O que deseja fazer?")
+    if op not in ['L', 'I', 'R', 'S', 'l', 'i', 'r', 's']:
+        print("Opção inválida!")
+        continue
+
+    if op == 's' or op == 'S':
+        break
+
+conn.close()
