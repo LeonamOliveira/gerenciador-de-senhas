4cfa45372fbb427b47335be49a97ab350272ff3f
LeonamOliveira <oliveira.leonam@hotmail.com>
2021-06-23 06:37:53 -0300

    Finalizando o projeto

--- password.py
+++ password.py
@@ -13,6 +13,7 @@ cursor = conn.cursor()
 
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS users(
+        service TEXT NOT NULL,
         username TEXT NOT NULL,
         password TEXT NOT NULL
     );
@@ -28,6 +29,35 @@ def menu():
     print("********************************")
 
 
+def get_password(service):
+    cursor.execute(f'''
+        SELECT username, password FROM users
+        WHERE service = '{service}'
+    ''')
+
+    if cursor.rowcount == 0:
+        print("Serviço não cadastrado (use 'l' para verificar os serviços).")
+    else:
+        for user in cursor.fetchall():
+            print(user)
+
+
+def insert_password(service, username, password):
+    cursor.execute(f'''
+        INSERT INTO users (service, username, password) 
+        VALUES ('{service}', '{username}', '{password}')
+    ''')
+    conn.commit()
+
+
+def show_services():
+    cursor.execute('''
+        SELECT service FROM users;
+    ''')
+    for service in cursor.fetchall():
+        print(service)
+
+
 while True:
     menu()
     op = input("O que deseja fazer?")
@@ -37,5 +67,18 @@ while True:
 
     if op == 's' or op == 'S':
         break
+
+    if op == 'i' or op == 'I':
+        service = input("Qual o nome do serviço?")
+        username = input("Qual o nome do usuário?")
+        password = input("Qual a senha?")
+        insert_password(service, username, password)
+
+    if op == "l" or op == "L":
+        show_services()
+
+    if op == "r" or op == "R":
+        service = input("Qual o serviço para o qual quer a senha?")
+        get_password(service)
 
 conn.close()
